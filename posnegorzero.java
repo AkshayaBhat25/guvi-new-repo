c1ab18e0b73de98ad719d715dc652214bc11f06c
AkshayaBhat25 <bhatlakshaya@gmail.com>
2017-03-09 16:14:55 +0530

    Update posnegorzero.java

--- posnegorzero.java
+++ posnegorzero.java
@@ -9,17 +9,12 @@ class Ideone
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-	
 		int testNum = 25253;
 		 if(testNum > 1){
-		 	 testNum = testNum % 100;
 			 System.out.println("Number is Positive");
-			 System.out.println("Output :"+ testNum);
 		 }
  		else {
- 			 testNum = 0;
 			 System.out.println("Number is Negatve");
-			 System.out.println("Output :"+ testNum);
 		}
 	}
 }
